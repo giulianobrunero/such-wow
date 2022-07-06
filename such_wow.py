a038bac1684bc8e22da1c25c15c4a7c743cf0348
giuliano brunero <giuliano.brunero@gmail.com>
2022-07-05 21:48:57 -0300

    Agrego paso de parametros por terminal. Funciona correctamente la logica.

--- such_wow.py
+++ such_wow.py
@@ -1,10 +1,11 @@
-
+import sys
 
 def such_wow(initial, final):
 	l = [i for i in range(initial, final+1)]
 	lp = [('SuchWow' if (n%3==0 and n%5==0) else 'Such' if (n%3==0) else 'Wow' if (n%5==0) else n) for n in l]
 	return lp
 
-initial = 1
-final = 10
+initial = int(sys.argv[1])
+final= int(sys.argv[2])
+
 print(such_wow(initial, final))
